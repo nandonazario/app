812af98a7a0e560aac5ce8ab86f69bc71212ed0c
Fernando <nando_nazario2@hotmail.com>
2013-08-26 11:13:46 -0300

    commit inicial

---
+++ App/src/com/betha/brans/InicioBean.java
@@ -0,0 +1,17 @@
+package com.betha.brans;
+
+public class InicioBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void enviar(){
+		
+	}
+}
